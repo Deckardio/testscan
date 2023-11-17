553bf2d67db9fb5ac697ee7750b00f33c56e6715
Sergei <47296872+Deckardio@users.noreply.github.com>
2023-11-17 16:24:44 +0400

    Create scan.sh

---
+++ scan.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+IP_ADDRESS=$1
+PORTS=$(seq 1 1000)
+
+scan_port() {
+    local ip=$1
+    local port=$2
+    timeout 1 bash -c "echo > /dev/tcp/$ip/$port" 2>/dev/null && echo "Port $port is open" || echo "Port $port is closed"
+}
+
+if [ -z "$IP_ADDRESS" ]; then
+    echo "Usage: bash scan.sh [IP_ADDRESS]"
+    exit 1
+fi
+
+for port in $PORTS; do
+    scan_port $IP_ADDRESS $port
+done
